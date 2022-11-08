e5b9d4aa750532d0b1b410d00208c04a1c44a4e0
nwmweranwm <116069235+nwmweranwm@users.noreply.github.com>
2022-11-08 10:40:19 +0100

    Create program.py

---
+++ program.py
@@ -0,0 +1,18 @@
+def min_max(list):
+
+    result = []
+    if len(list) == 0:
+        return None
+    min = list[0]
+    max = list[0]
+    for element in list:
+        if element > max:
+            max = element
+        if element < min:
+            min = element
+
+    result.append(min)
+    result.append(max)
+    return result
+
+print(min_max([6, 8, 78, 2]))
